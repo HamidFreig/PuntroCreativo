b15edf37235659ec1231aafe311eb80d668faad9
Hamid Freig <110936554+HamidFreig@users.noreply.github.com>
2025-01-04 16:51:50 -0300

    se inicialia proyecto puntocreativo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="text-blue-800">Texto Azul</p>
+      <p className="text-green-800">Texto Verde</p>
+      <p className="text-red-800">Texto Rojo</p>
+      <p className="text-black">Texto Negro</p>
+    </div>
+  );
+}
+
+export default App;
